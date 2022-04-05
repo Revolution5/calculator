375e73975d4dd201097c55644e7118bd80dc5a2c
Revolution5 <damonboone99@gmail.com>
2022-04-05 16:47:08 -0700

    Get first number and activate clear button

--- calc.js
+++ calc.js
@@ -37,17 +37,39 @@ function displayOnScreen(value) {
 }
 
 
-let input = ""
-const btns = Array.from(document.querySelectorAll(".btn"));
-btns.forEach(btn => {
-    btn.addEventListener("click", function(e) {
-        //if the button is a number, put its content in input and display it
-        if(btn.classList.contains("num")){
-            input += btn.textContent;
-            displayOnScreen(input)
-        }
-        //if the button is an operator, save that as a seperate input
+let input = "";
+let firstNum = 0;
+let secondNum = 0;
+let opr = "";
+
+
+function getFirstNumber() {
+    const numBtns = Array.from(document.querySelectorAll(".num"));
+    numBtns.forEach(numBtn => {
+        numBtn.addEventListener("click", function(e) {
+            input += numBtn.textContent;
+            if(input.length > 10) {
+                input = input.substring(0, 10);
+            }
+            displayOnScreen(input);
+            firstNum = parseInt(input)
+        })
     })
-})
+    return firstNum;
+}
+
+function activateClearButton() {
+    const clearbtn = document.querySelector(".clear");
+    clearbtn.addEventListener("click", function(e) {
+        input = "";
+        firstNum = 0;
+        secondNum = 0;
+        opr = "";
+        displayOnScreen(0);
+    })
+}
+
+getFirstNumber();
+activateClearButton();
 
 
